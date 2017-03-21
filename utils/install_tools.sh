advancetoolchain/advance-toolchain

commit 5e3744c05d98eccca4dd283ed1dfd3ea3c61ba58

Fix install_tools.sh to be able to install for one target arch
This patch modifies install_tools.sh to make possible install DEB
packages for only one target on cross compilers.

Signed-off-by: Raphael Moreira Zinsly <rzinsly@linux.vnet.ibm.com>

utils/install_tools.sh
@@ -55,8 +55,7 @@ install_cross ()
 {
 	if [[ ${debian} == "yes" ]]; then
 		sudo dpkg -i advance-toolchain-at*common* || return ${?}
-		sudo dpkg -i advance-toolchain-at*cross-ppc64_* \
-			advance-toolchain-at*cross-ppc64le_* || return ${?}
+		ls *at* | egrep "64_|64le_" | xargs sudo dpkg -i || return ${?}
 		sudo dpkg -i advance-toolchain-at*cross-ppc*mcore* \
 			advance-toolchain-at*cross-ppc*extras* || return ${?}
 	else

commit ff9dc2cfe918915f18c310b3b6498c7baaadf187

Filter DEB packages when using install_tools.sh on cross.
Commit 5e3744c05d98eccca4dd283ed1dfd3ea3c61ba58 introduced a bug if
the user has other AT files in the same directory, this patch filters
for DEB packages to avoid that.

Signed-off-by: Raphael Moreira Zinsly <rzinsly@linux.vnet.ibm.com>

utils/install_tools.sh
@@ -55,7 +55,8 @@ install_cross ()
 {
 	if [[ ${debian} == "yes" ]]; then
 		sudo dpkg -i advance-toolchain-at*common* || return ${?}
-		ls *at* | egrep "64_|64le_" | xargs sudo dpkg -i || return ${?}
+		ls *at*.deb | egrep "64_|64le_" | xargs sudo dpkg -i \
+			|| return ${?}
 		sudo dpkg -i advance-toolchain-at*cross-ppc*mcore* \
 			advance-toolchain-at*cross-ppc*extras* || return ${?}
 	else