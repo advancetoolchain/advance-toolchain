ab51a28d0ed6e4568eac20d005471e04a2604a13
Erwan Prioul <erwan@linux.ibm.com>
2018-07-25 17:00:07 +0200

    Add requirement for GNU make on AT 12.0
    GNU make 4.0 (or greater) is required by glibc 2.28 which is part
    of AT 12.0 (or greater).
    
    Signed-off-by: Erwan Prioul <erwan@linux.ibm.com>

--- configs/12.0/distros/suse-12.mk
+++ configs/12.0/distros/suse-12.mk
@@ -75,7 +75,7 @@ ifndef AT_DISTRO_REQ_PKGS
     AT_COMMON_PKGS_REQ := zlib-devel ncurses-devel flex bison texinfo \
                           createrepo subversion cvs gawk autoconf rsync curl \
                           bc automake rpm-build gcc-c++ xorg-x11-util-devel \
-                          docbook2x wget autoconf-archive
+                          docbook2x wget autoconf-archive make
     AT_CROSS_PGMS_REQ :=
     AT_NATIVE_PGMS_REQ :=
     AT_COMMON_PGMS_REQ := git_1.7
