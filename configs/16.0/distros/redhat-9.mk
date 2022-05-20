ae00e70300b1f84a1ebae45d01ea9ccbf4143f26
Matheus Castanho <msc@linux.ibm.com>
2022-05-20 14:50:15 -0300

    Require perl-FindBin on RHEL9
    This package is needed to build OpenSSL.
    
    Signed-off-by: Matheus Castanho <msc@linux.ibm.com>

--- configs/16.0/distros/redhat-9.mk
+++ configs/16.0/distros/redhat-9.mk
@@ -75,7 +75,7 @@ ifndef AT_DISTRO_REQ_PKGS
                           autoconf bzip2-[0-9] libtool-[0-9] \
                           gzip rpm-build-[0-9] rpm-sign gcc-c++ wget \
                           docbook2X autoconf-archive libffi-devel python3 \
-                          systemtap-sdt-devel git make
+                          systemtap-sdt-devel git make perl-FindBin
     AT_CROSS_PGMS_REQ :=
     AT_NATIVE_PGMS_REQ :=
     AT_COMMON_PGMS_REQ :=
