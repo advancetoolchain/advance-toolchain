c7c468117e0c720a4fe049ebbeac4fb92b41428d
Erwan Prioul <erwan@linux.ibm.com>
2022-09-05 15:18:24 +0200

    Replace AT 13.0 symlinks with files
    Prepare for removal of older AT configuration.
    
    Signed-off-by: Erwan Prioul <erwan@linux.ibm.com>

--- configs/13.0/packages/tcmalloc/tcmalloc.mk
+++ configs/13.0/packages/tcmalloc/tcmalloc.mk
@@ -1,1 +1,33 @@
-../../../12.0/packages/tcmalloc/tcmalloc.mk
+# Copyright 2017 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+#
+$(eval $(call set_provides,tcmalloc_1,multi,cross_yes))
+
+tcmalloc: $(RCPTS)/tcmalloc_1.rcpt
+
+# Add it to the target 3rdparty_libs
+3rdparty_libs-reqs += $(RCPTS)/tcmalloc_1.rcpt
+
+$(RCPTS)/tcmalloc_1.rcpt: $(tcmalloc_1-archdeps)
+	@touch $@
+
+$(RCPTS)/tcmalloc_1-32.a.rcpt: $(patsubst %,$(RCPTS)/%.rcpt,gcc_4 \
+                                                            rsync_tcmalloc)
+	@touch $@
+
+$(RCPTS)/tcmalloc_1-64.a.rcpt: $(patsubst %,$(RCPTS)/%.rcpt,gcc_4 \
+                                                            rsync_tcmalloc)
+	@touch $@
