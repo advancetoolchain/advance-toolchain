dcbc21c867bea03acd97422ea5217f0d6cd99cf6
Erwan Prioul <erwan@linux.ibm.com>
2022-09-05 15:18:24 +0200

    Replace AT 13.0 symlinks with files
    Prepare for removal of older AT configuration.
    
    Signed-off-by: Erwan Prioul <erwan@linux.ibm.com>

--- configs/13.0/packages/gmp/gmp.mk
+++ configs/13.0/packages/gmp/gmp.mk
@@ -1,1 +1,44 @@
-../../../12.0/packages/gmp/gmp.mk
+# Copyright 2017 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+# In a native build, the first stage of this packages is created using the
+# system toolchain.
+# As soon as the new toolchain is fully featured, we rebuild this package.
+# In a cross compiler, this package is built for the host architecture,
+# using the system toolchain, which is already fully featured.  So, we don't
+# need to rebuild it.
+#
+$(eval $(call set_provides,gmp_1,single,cross_yes))
+$(eval $(call set_provides,gmp_2,single,cross_no))
+
+
+gmp_1: $(RCPTS)/gmp_1.rcpt
+
+gmp_2: $(RCPTS)/gmp_2.rcpt
+
+
+# Build it using the system toolchain
+$(RCPTS)/gmp_1.rcpt: $(gmp_1-archdeps)
+	@touch $@
+
+# Rebuild it using the new fully featured toolchain
+$(RCPTS)/gmp_2.rcpt: $(gmp_2-archdeps)
+	@touch $@
+
+
+$(RCPTS)/gmp_1.a.rcpt: $(RCPTS)/rsync_gmp.rcpt
+	@touch $@
+
+$(RCPTS)/gmp_2.a.rcpt: $(RCPTS)/binutils_2.rcpt
+	@touch $@
--- configs/13.0/packages/openssl/openssl.mk
+++ configs/13.0/packages/openssl/openssl.mk
@@ -1,1 +1,42 @@
-../../../12.0/packages/openssl/openssl.mk
+# Copyright 2017 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+#
+$(eval $(call set_provides,openssl_1,multi,cross_yes))
+
+# List of dependencies in order to build the tuned libraries for 32 or
+# 64 bits.
+openssl_tuned-32-deps := $(RCPTS)/gcc_4.rcpt \
+                         $(RCPTS)/rsync_openssl.rcpt
+openssl_tuned-64-deps := $(RCPTS)/gcc_4.rcpt \
+                         $(RCPTS)/rsync_openssl.rcpt
+# Enable tuned targets
+$(eval $(call provide_tuneds,openssl))
+
+openssl_1: $(RCPTS)/openssl_1.rcpt
+
+openssl_tuned: $(RCPTS)/openssl_tuned.rcpt
+
+$(RCPTS)/openssl_1.rcpt: $(openssl_1-archdeps)
+	@touch $@
+
+$(RCPTS)/openssl_1-32.a.rcpt: $(patsubst %,$(RCPTS)/%.rcpt,gcc_4 zlib_1 rsync_openssl)
+	@touch $@
+
+$(RCPTS)/openssl_1-64.a.rcpt: $(patsubst %,$(RCPTS)/%.rcpt,gcc_4 zlib_1 rsync_openssl)
+	@touch $@
+
+$(RCPTS)/openssl_tuned.rcpt: $(openssl_tuned-archdeps)
+	@touch $@
