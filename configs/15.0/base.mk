ed4e2ae858365f828be7b2e060b00550ddb45f51
Erwan Prioul <erwan@linux.ibm.com>
2023-01-26 16:08:04 +0100

    Prepare for AT 15.0-4
    This patch also updates the release notes of AT 15.0 for the 15.0-4 release.
    
    Signed-off-by: Erwan Prioul <erwan@linux.ibm.com>

--- configs/15.0/base.mk
+++ configs/15.0/base.mk
@@ -49,7 +49,7 @@
 AT_NAME := at
 AT_MAJOR_VERSION := 15.0
 AT_REVISION_NUMBER := 4
-AT_INTERNAL := rc2
+AT_INTERNAL := none
 AT_PREVIOUS_VERSION := 14.0
 
 # Minimum kernel version distributed on supported distros by this AT version,
