887db51380078c92150e8c7df17ee7e65758d177
Erwan Prioul <erwan@linux.ibm.com>
2023-08-02 11:55:37 +0200

    Prepare for AT 17.0-0 Beta4
    Signed-off-by: Erwan Prioul <erwan@linux.ibm.com>

--- configs/17.0/base.mk
+++ configs/17.0/base.mk
@@ -49,7 +49,7 @@
 AT_NAME := at
 AT_MAJOR_VERSION := 17.0
 AT_REVISION_NUMBER := 0
-AT_INTERNAL := beta3
+AT_INTERNAL := beta4
 AT_PREVIOUS_VERSION := 16.0
 
 # Minimum kernel version distributed on supported distros by this AT version,
