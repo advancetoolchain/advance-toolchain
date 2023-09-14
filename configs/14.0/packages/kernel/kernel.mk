49e4a2e3e0732e4dcd93bf866eee56755fb3dd2d
Erwan Prioul <erwan@linux.ibm.com>
2023-09-14 14:06:59 +0200

    Replace AT 14.0 symlinks with files
    Prepare for removal of older AT configuration.
    
    Signed-off-by: Erwan Prioul <erwan@linux.ibm.com>

--- configs/14.0/packages/kernel/kernel.mk
+++ configs/14.0/packages/kernel/kernel.mk
@@ -1,1 +1,25 @@
-../../../13.0/packages/kernel/kernel.mk
+# Copyright 2017 IBM Corporation
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#    http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+#
+#
+$(eval $(call set_provides,kernel_h,single,cross_yes))
+
+kernel_h: $(RCPTS)/kernel_h.rcpt
+
+$(RCPTS)/kernel_h.rcpt: $(kernel_h-archdeps)
+	@touch $@
+
+$(RCPTS)/kernel_h.a.rcpt: $(RCPTS)/rsync_kernel.rcpt
+	@touch $@
