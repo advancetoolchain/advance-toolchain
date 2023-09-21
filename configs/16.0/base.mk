808365931d02ba3d53d06447ad9c512b9263582b
Erwan Prioul <erwan@linux.ibm.com>
2023-09-21 15:46:59 +0200

    Prepare for AT 16.0-3
    This patch also updates the release notes of AT 16.0 for the 16.0-3 release.
    
    Signed-off-by: Erwan Prioul <erwan@linux.ibm.com>

--- configs/16.0/base.mk
+++ configs/16.0/base.mk
@@ -49,7 +49,7 @@
 AT_NAME := at
 AT_MAJOR_VERSION := 16.0
 AT_REVISION_NUMBER := 3
-AT_INTERNAL := rc2
+AT_INTERNAL := none
 AT_PREVIOUS_VERSION := 15.0
 
 # Minimum kernel version distributed on supported distros by this AT version,
