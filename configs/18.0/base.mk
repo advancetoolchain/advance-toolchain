a892da22934d4b0aa4c71039c9dac17cc6c79725
Erwan Prioul <erwan@linux.ibm.com>
2024-06-26 11:21:54 +0200

    Prepare for AT 18.0-0 Alpha1
    Signed-off-by: Erwan Prioul <erwan@linux.ibm.com>

--- configs/18.0/base.mk
+++ configs/18.0/base.mk
@@ -49,7 +49,7 @@
 AT_NAME := at
 AT_MAJOR_VERSION := 18.0
 AT_REVISION_NUMBER := 0
-AT_INTERNAL := alpha
+AT_INTERNAL := alpha1
 AT_PREVIOUS_VERSION := 17.0
 
 # Minimum kernel version distributed on supported distros by this AT version,
